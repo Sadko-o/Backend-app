23549c8cf054859ab5694f213a3246660b4caf31
Sadko-o <r.saduakhas@gmail.com>
2022-11-23 15:53:37 +0600

    commit

--- HospitalManagementApp/models.py
+++ HospitalManagementApp/models.py
@@ -1,49 +1,48 @@
-from django.db import models
+# from django.db import models
 
-# Create your models here.
+# # Create your models here.
 
-class DiseaseType(models.Model):
-    diseaseTypeId = models.IntegerField(primary_key=True, default='0')
-    diseaseTypeDescription = models.CharField(max_length=140)
+# class DiseaseType(models.Model):
+#     diseaseTypeId = models.IntegerField(primary_key=True, default='0')
+#     diseaseTypeDescription = models.CharField(max_length=140)
 
-class Disease(models.Model):
-    disease_code = models.CharField(max_length=50, primary_key=True)
-    pathogen = models.CharField(max_length=20)
-    description = models.CharField(max_length=140)
-    diseaseTypeId = models.ForeignKey(DiseaseType, on_delete=models.CASCADE)
+# class Disease(models.Model):
+#     disease_code = models.CharField(max_length=50, primary_key=True)
+#     pathogen = models.CharField(max_length=20)
+#     description = models.CharField(max_length=140)
+#     diseaseTypeId = models.ForeignKey(DiseaseType, on_delete=models.CASCADE)
 
-class Country(models.Model):
-    cname = models.CharField(max_length=50, primary_key=True)
-    population = models.BigIntegerField()
+# class Country(models.Model):
+#     cname = models.CharField(max_length=50, primary_key=True)
+#     population = models.BigIntegerField()
 
-class Discover(models.Model):
-    cname = models.OneToOneField(Country, models.DO_NOTHING, primary_key=True)
-    disease_code = models.OneToOneField(Disease, models.DO_NOTHING)
-    first_enc_date = models.DateField()
+# class Discover(models.Model):
+#     cname = models.OneToOneField(Country, models.DO_NOTHING, primary_key=True)
+#     disease_code = models.OneToOneField(Disease, models.DO_NOTHING)
+#     first_enc_date = models.DateField()
 
-class Users(models.Model):
-    email = models.CharField(max_length=60, primary_key=True)
-    name = models.CharField(max_length=30)
-    surname = models.CharField(max_length=40)
-    salary = models.IntegerField()
-    phone = models.CharField(max_length=20)
-    cname = models.ForeignKey(Country, on_delete=models.CASCADE)
+# class Users(models.Model):
+#     email = models.CharField(max_length=60, primary_key=True)
+#     name = models.CharField(max_length=30)
+#     surname = models.CharField(max_length=40)
+#     salary = models.IntegerField()
+#     phone = models.CharField(max_length=20)
+#     cname = models.ForeignKey(Country, on_delete=models.CASCADE)
+# class PublicServant(models.Model):
+#     email = models.OneToOneField(Users, models.DO_NOTHING, primary_key=True)
+#     department = models.CharField(max_length=50)
 
-class PublicServant(models.Model):
-    email = models.OneToOneField(Users, models.DO_NOTHING, primary_key=True)
-    department = models.CharField(max_length=50)
+# class Doctor(models.Model):
+#     email = models.OneToOneField(Users, models.DO_NOTHING, primary_key=True)
+#     degree = models.CharField(max_length=20)
 
-class Doctor(models.Model):
-    email = models.OneToOneField(Users, models.DO_NOTHING, primary_key=True)
-    degree = models.CharField(max_length=20)
+# class Specialize(models.Model):
+#     diseaseTypeId = models.ForeignKey(DiseaseType, on_delete=models.CASCADE)
+#     email = models.ForeignKey(Doctor, on_delete=models.CASCADE)
 
-class Specialize(models.Model):
-    diseaseTypeId = models.ForeignKey(DiseaseType, on_delete=models.CASCADE)
-    email = models.ForeignKey(Doctor, on_delete=models.CASCADE)
-
-class Record(models.Model):
-    email = models.ForeignKey(PublicServant, on_delete=models.CASCADE)
-    cname = models.ForeignKey(Country, on_delete=models.CASCADE)
-    disease_code = models.ForeignKey(Disease, on_delete=models.CASCADE)
-    total_deaths = models.IntegerField()
-    total_patients = models.IntegerField()
+# class Record(models.Model):
+#     email = models.ForeignKey(PublicServant, on_delete=models.CASCADE)
+#     cname = models.ForeignKey(Country, on_delete=models.CASCADE)
+#     disease_code = models.ForeignKey(Disease, on_delete=models.CASCADE)
+#     total_deaths = models.IntegerField()
+#     total_patients = models.IntegerField()
